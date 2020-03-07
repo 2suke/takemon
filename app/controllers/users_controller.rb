7120be9301a48f5ad3cd43e2e149837318bd898b
2suke <takozawa3@gmail.com>
2020-03-07 12:42:39 +0900

    add login and logout functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = 'ユーザーを登録しました。確認メールを送付したので、アカウントを有効化してください。'
-      redirect_to 
+      redirect_to @user
     else
       render 'new'
     end
