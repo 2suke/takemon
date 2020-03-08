0790e5ba13edf196c2a72db22b8f88f85fb9995b
2suke <takozawa3@gmail.com>
2020-03-08 16:28:26 +0900

    add uniquness param to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGIX }, uniquness: true
 end
