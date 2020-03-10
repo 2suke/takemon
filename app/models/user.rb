2fcbbea71490a68ffcaf98a0c39566d645ecb15f
2suke <takozawa3@gmail.com>
2020-03-10 10:06:38 +0900

    fix user model and session view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGIX }, uniquness: true
+            format: { with: VALID_EMAIL_REGIX }, uniqueness: true
 end
