54821a0eb20a15c9a6070f6e98e521dc4640c31e
2suke <takozawa3@gmail.com>
2020-05-02 01:13:21 +0900

    fix header toggle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @portfolio = @user.portfolios.all
+    @portfolios = @user.portfolios.all
   end
 
   def index
