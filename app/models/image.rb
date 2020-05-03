06d206ca5e98210726586f1d59ddd8a324298841
2suke <takozawa3@gmail.com>
2020-05-04 07:43:45 +0900

    fix image model valid → validates

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :portfolio, optional: true
   mount_uploader :image, ImageUploader
-  valid :portfolio_id, presence: true
-  valid :image, presence: true
+  validates :portfolio_id, presence: true
+  validates :image, presence: true
 end
