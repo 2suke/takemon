f72f83bd2a13532326187c16118b67d9b131d5f6
2suke <takozawa3@gmail.com>
2020-07-01 04:09:06 +0900

    add favorite spec

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -7,4 +7,12 @@ module AuthenticationHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def e2elogin(user)
+    visit '/login'
+
+    fill_in 'session_email', with: user.email
+    fill_in 'session_password', with: user.password
+    click_on 'commit'
+  end
 end
