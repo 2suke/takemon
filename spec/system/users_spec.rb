ea7f08d9d8db733f74191d2e4f9d5c19dc0c96ee
2suke <takozawa3@gmail.com>
2020-07-04 05:40:42 +0900

    tmp commit for e2e merge

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'Users' do
-  let(:valid_user) { build(:valid_user) }
+  let(:valid_user)  { build(:valid_user) }
+  let(:common_user) { create(:valid_user) }
+  let(:admin_user)  { create(:admin_user) }
 
   scenario 'new valid user sign up' do
     visit '/users/new'
@@ -14,4 +16,13 @@ feature 'Users' do
 
     expect(page).to have_content 'ユーザーを登録しました。確認メールを送付したので、アカウントを有効化してください。'
   end
+
+  # scenario 'common user can not delete user' do
+  #   login common_user
+
+  #   visit users_path
+
+  #   expect(page).to_not have_content 'delete'
+  #   expect(page).to_not have_link    'delete'
+  # end
 end
